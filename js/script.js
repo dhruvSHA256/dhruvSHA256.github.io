a485befee82fe846bc4f578277ba30af690484ec
DHruv <dhruv.sha256@gmail.com>
2023-11-07 06:53:46 +0530

    Auto commit - Tue Nov  7 06:53:46 AM IST 2023

--- js/script.js
+++ js/script.js
@@ -1,21 +1,62 @@
-const setTheme = (theme) => {
+// https://github.com/mermaid-js/mermaid/issues/1945
+function setTheme(theme) {
   document.documentElement.className = theme;
-  localStorage.setItem('theme', theme);
+  localStorage.setItem("theme", theme);
+  if (theme === "dark") {
+    document.body.dispatchEvent(new CustomEvent("dark-theme-set"));
+    changeGiscusTheme("noborder_dark");
+  } else {
+    document.body.dispatchEvent(new CustomEvent("light-theme-set"));
+    changeGiscusTheme("noborder_light");
+  }
 }
 
-const hasCodeRun = localStorage.getItem('hasCodeRun');
+function modeSwitcher(theme) {
+  if (theme == "dark") {
+    setTheme("dark");
+  } else {
+    setTheme("light");
+  }
+}
+
+const hasCodeRun = localStorage.getItem("hasCodeRun");
 
 if (!hasCodeRun) {
   const defaultTheme = "{{ config.extra.default_theme }}";
   setTheme(defaultTheme);
-  localStorage.setItem('hasCodeRun', 'true');
+  localStorage.setItem("hasCodeRun", "true");
 }
 
 const getTheme = () => {
-  const theme = localStorage.getItem('theme');
+  const theme = localStorage.getItem("theme");
   if (theme) {
     setTheme(theme);
   }
+};
+
+// https://github.com/giscus/giscus/issues/336#issuecomment-1007922777
+function changeGiscusTheme(theme) {
+  function sendMessage(message) {
+    const iframe = document.querySelector("iframe.giscus-frame");
+    if (!iframe) return;
+    iframe.contentWindow.postMessage({ giscus: message }, "https://giscus.app");
+  }
+  sendMessage({
+    setConfig: {
+      theme: theme,
+    },
+  });
 }
+
+const globalColorMode = localStorage.getItem("theme")
+  ? "noborder_dark"
+  : "noborder_light";
+function handleGiscusMessage(event) {
+  if (event.origin !== "https://giscus.app") return;
+  if (!(typeof event.data === "object" && event.data.giscus)) return;
+  changeGiscusTheme(globalColorMode);
+}
+
+window.addEventListener("message", handleGiscusMessage);
 
 getTheme();
