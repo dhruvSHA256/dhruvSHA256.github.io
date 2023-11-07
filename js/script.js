10ae19b76092f047343eedbd9d6319fbb673630b
DHruv <dhruv.sha256@gmail.com>
2023-11-07 17:51:57 +0530

    Auto commit - Tue Nov  7 05:51:57 PM IST 2023

--- js/script.js
+++ js/script.js
@@ -1,21 +1,51 @@
 const setTheme = (theme) => {
   document.documentElement.className = theme;
-  localStorage.setItem('theme', theme);
-}
+  localStorage.setItem("theme", theme);
+  if (theme == "dark") {
+    changeGiscusTheme("noborder_dark");
+  } else {
+    changeGiscusTheme("noborder_light");
+  }
+};
 
-const hasCodeRun = localStorage.getItem('hasCodeRun');
+const hasCodeRun = localStorage.getItem("hasCodeRun");
 
 if (!hasCodeRun) {
-  const defaultTheme = "{{ config.extra.default_theme }}";
+  const defaultTheme = "dark";
   setTheme(defaultTheme);
-  localStorage.setItem('hasCodeRun', 'true');
+  localStorage.setItem("hasCodeRun", "true");
 }
 
 const getTheme = () => {
-  const theme = localStorage.getItem('theme');
+  const theme = localStorage.getItem("theme");
   if (theme) {
     setTheme(theme);
   }
+};
+
+function changeGiscusTheme(theme) {
+  function sendMessage(message) {
+    const iframe = document.querySelector("iframe.giscus-frame");
+    if (!iframe) return;
+    iframe.contentWindow.postMessage({ giscus: message }, "https://giscus.app");
+  }
+  sendMessage({
+    setConfig: {
+      theme: theme,
+    },
+  });
 }
+
+function handleGiscusMessage(event) {
+  if (event.origin !== "https://giscus.app") return;
+  if (!(typeof event.data === "object" && event.data.giscus)) return;
+  let globalColorMode =
+    localStorage.getItem("theme") == "dark"
+      ? "noborder_dark"
+      : "noborder_light";
+  changeGiscusTheme(globalColorMode);
+}
+
+window.addEventListener("message", handleGiscusMessage);
 
 getTheme();
